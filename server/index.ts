6199136f0b7f62a43b93e6d95b81b265ef77f116
anudeepgunna <anudeepgunna18@gmail.com>
2024-07-20 20:26:07 +0530

    Updating package.json

--- server/index.ts
+++ server/index.ts
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.BASE_URL,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     credentials: true,
     allowedHeaders: [
